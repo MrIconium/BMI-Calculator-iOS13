5848528dcfb7d54649f9bbe2bb70b045b9990acb
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-06-14 17:12:25 +0300

    Apply solution

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Mehmet Emrah Konya on 14.06.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
